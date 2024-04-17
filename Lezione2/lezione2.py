03752355fea0649e672fa127e004698375e56b4b
Ricky36002 <rrcyber8@gmail.com>
2024-04-17 13:53:19 +0200

    Aggiunta lezione2

---
+++ Lezione2/lezione2.py
@@ -0,0 +1,12 @@
+#Riccardo Russo
+# 17/04/2024
+
+#Use a variable to represent a person's name,
+#and print a message to that person.
+#Your message should be simple, such as,
+#"Hello eric, would you like to lear some python today?"
+print ("Hello!")
+
+name: str = "Giggio"
+message: str = f"Ciao {name} ti va di mangiare un boccone?"
+print (message)
