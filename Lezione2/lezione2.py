62dc764d0713cfe7ec64d28913345a3f98637e49
Ricky36002 <rrcyber8@gmail.com>
2024-04-22 13:47:48 +0200

    Commit

--- Lezione2/lezione2.py
+++ Lezione2/lezione2.py
@@ -402,6 +402,10 @@ for dizionario in City_Dictionary:
     for k, v in dizionario.items():
         print(f"{k}\n{v}")
     print ("")
+
+
+# We’re now working with examples that are complex enough that they can be extended in any number of ways. Use one of the example
+#programs from this chapter, and extend it by adding new keys and values,changing the context of the program, or improving the formatting of the output.
         
 
 
